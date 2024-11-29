0e6bbb8050f2993f359cf8b5b096993b535f1154
tayanPinto <taylanlaflamme@gmail.com>
2024-11-28 21:47:13 -0300

    Prova at

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="container-home">
+      <label>Home</label>
+      <h1>Total Entrada:</h1>
+      <h1>Total Saída:</h1>
+      <h1>Saldo Atual:</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Transacao.js
@@ -0,0 +1,12 @@
+import Tabela from '../Components/Tabela';
+
+const Transacao = () => {
+  return (
+    <div>
+      <h1>Transaçoes</h1>
+      <Tabela />
+    </div>
+  );
+};
+
+export default Transacao;
